eb14929cf7cdf2fac03f8cebb558fca1cbac8866
Nikita <p3trenk0n@gmail.com>
2018-01-27 23:02:37 -0500

    Add category view

---
+++ Todoey/Controllers/CategoryViewController.swift
@@ -0,0 +1,28 @@
+//
+//  CategoryViewController.swift
+//  Todoey
+//
+//  Created by Nikita Petrenko on 1/27/18.
+//  Copyright © 2018 Nikita Petrenko. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CategoryViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+    }
+    
+    //MARK: - TableView Datasource Methods
+    
+    //MARK: - TableView Delegate Methods
+    
+    //MARK: - Data Manipulation Methods
+    
+}
